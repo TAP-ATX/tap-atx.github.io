f2c25079c0d00d2047ef9f9641e377f3b6bb80d6
Justin Chang <jchang691@gmail.com>
2022-08-12 15:22:47 -0500

    Adds embedded calendar

--- src/pages/home.js
+++ src/pages/home.js
@@ -89,6 +89,17 @@ const Home = () => {
           <hr />
         </Container>
         <Container className="info-container text-center">
+          <h1 className="header-title">Upcoming Events</h1>
+          <iframe
+            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23009c82&ctz=America%2FChicago&showTitle=1&showNav=1&showPrint=0&showDate=1&showCalendars=0&showTabs=0&showTz=0&src=aW5hbWRlbjUwcXJuNDZiYzZua3Mya3ZsaDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23D81B60"
+            style={{ borderWidth: 0 }}
+            width="800"
+            height="600"
+            frameBorder="0"
+            scrolling="no"
+          ></iframe>
+        </Container>
+        <Container className="info-container text-center">
           <h1 className="header-title">Support Us</h1>
           <p className="support-text">
             Your contributions will help us continue to create and plan events and programs!
